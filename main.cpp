653f5db36ef7af84ff4838940846af681de01dad
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-02-20 12:11:04 +0000

    - Added files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//Write a recursive function divide( int ) that will take an int argument and will
+//repeatedly divide the argument by 2 (integer division), by calling divide() recursively,
+//until the argument becomes less than 1. In the divide() function, output the value of the
+//argument each time the function is called.
+
+//Assume that the function divide() is called with an argument of 4. Draw a call stack with
+//stack frames showing the parameter variable for all method invocations at the point
+//when the terminating condition becomes true.
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
